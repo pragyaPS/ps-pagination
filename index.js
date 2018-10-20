f18793f1068b13cb9fc73238873060cfff1be022
Pragya Singh <pragyasingh991@gmail.com>
2018-10-20 11:35:01 +1100

    third commit

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 
 import Pagination from './src/pagination';
 
-const App = () => (<div>ps-pagination
-    <Pagination count={4} onClick = { onPaginationClick }/>
-</div>);
+// const App = () => (<div>ps-pagination
+//     <Pagination count={4} onClick = { onPaginationClick }/>
+// </div>);
 
-const onPaginationClick = (action) => {
-    console.log('action', action);
-}
+// const onPaginationClick = (action) => {
+//     console.log('action', action);
+// }
 
-ReactDOM.render(<App/>, document.getElementById('react-container'));
-// console.log("Pagination", Pagination);
-// export default Pagination;
+// ReactDOM.render(<App/>, document.getElementById('react-container'));
+ console.log("Pagination", Pagination);
+ export default Pagination;
  
